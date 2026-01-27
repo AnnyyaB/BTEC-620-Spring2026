81026b373be4224ef4aef218053800bfa7951784
Annyya Bryant <annbryant@claflin.edu>
2026-01-27 13:49:34 -0500

    Add files via upload

---
+++ Microarray-Analysis.R
@@ -0,0 +1,12 @@
+Name: A'nnyya Bryant, Date: 01/27/2026, Purpose: Microarray Analysis
+
+#Install Affymetrix library/Package
+
+#if(!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+#BiocManager::install("affy")
+
+#library(affy)
+
+#setwd("/Users/bryan/Downloads")
